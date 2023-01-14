45c8e8ecd980236ae8a047e941dfdcbd02b0e404
Abdullah Akbar <Akbar.A@etoncollege.org.uk>
2023-01-14 10:07:49 +0000

    CarCustomiser

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Akbar, Abdullah (RCH) on 14/01/2023.
+//
+
+import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+}
--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    let car =  Car(make:"Mazda", model:"MX-5", topSpeed: 125, acceleration:7.7, handling: 5)
+
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("""
+             Car Make: \(car.make)
+             Car Model: \(car.model)
+             Car topSpeed(125mph): \(car.topSpeed)
+             Car acceleration(0-60): \(car.acceleration)s
+             Car handling: \(car.handling)
+             """)
             .padding()
+            
     }
 }
 
